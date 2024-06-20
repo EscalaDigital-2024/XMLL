81ec805a3a5af4495667eb5a514b4be5a01816e1
EscalaDigital-2024 <gustavo.cruz@escala-inc.com>
2024-06-20 16:02:00 -0600

    Prueba modificando formato del archivo subir.php

--- subir.php
+++ subir.php
@@ -1,6 +1,10 @@
 <form action="leer.php" method="post" enctype="multipart/form-data">
-  <label for="file">Archivo:</label>
-  <input type="file" name="myfile" id="file"/><br>
-  <br/>
-  <input type="submit" name="submit" value="ENVIAR"/>
-  </form>
+	<label for="file">Archivo:</label>
+	<input type="file" name="myfile" id="file"/><br>
+	<br/>
+	<input type="submit" name="submit" value="ENVIAR"/>
+ </form>  
+ 
+<!----> 
+  
+  
